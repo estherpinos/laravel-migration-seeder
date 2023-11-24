63f19542f6fb5c52f0109542ad78297e67c6145b
estherpinos <estherpinos1@gmail.com>
2023-11-24 17:45:59 +0100

    Add [table migration]

--- database/migrations/2023_11_24_163906_create_trains_table.php
+++ database/migrations/2023_11_24_163906_create_trains_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('reference', 80);
+            $table->string('slug', 80)->unique();
+            $table->string('Azienda', 100);
+            $table->string('Partenza', 100);
+            $table->string('Stazione_di_arrivo', 80);
+            $table->time('Orario_di_partenza', 100);
+            $table->time('Orario_di_arrivo', 100);
+            $table->int('Codice_treno', 100);
+            $table->integerIncrements('carrozze');
+            $table->string('In_orario', 100);
+            $table->string('Cancellato', 100)->nullable();
             $table->timestamps();
         });
     }
